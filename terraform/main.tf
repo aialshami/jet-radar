0a5a7b463df7cd31ad7df43522516d671111baa1
aialshami <a.i.alshami@gmail.com>
2023-05-17 12:12:23 +0100

    rds, repositories built

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Defining provider to use and its versions
 terraform {
   required_providers {
     aws = {
@@ -15,10 +16,33 @@ variable "aws_access_key" {
 variable "aws_secret_key" {
   type = string
 }
+variable "username" {
+  type = string
+}
+variable "password" {
+  type = string
+}
 
 # Configure AWS provider
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = "eu-west-2"
+}
+
+# Use existing VPC
+data "aws_vpc" "c7-vpc" {
+  id = "vpc-010fd888c94cf5102"
+}
+
+# Use existing subnet
+data "aws_db_subnet_group" "c7-subnets" {
+  name = "c7-db-subnet-group"
+}
+
+# Use existing security group
+data "aws_security_group" "c7-remote-access" {
+  name   = "c7-remote-access"
+  vpc_id = data.aws_vpc.c7-vpc.id
+  id     = "sg-01745c9fa38b8ed68"
 }
