1d04c0b3491f12363b0db9d6b62072b66a070fa6
Oliver Jordan <o.jordan2308@gmail.com>
2023-05-17 10:50:05 +0100

    complete haversine distance function

--- transform/transform.py
+++ transform/transform.py
@@ -1,7 +1,9 @@
 import json
+from math import sin, cos, acos, asin, sqrt, pi
 
 
 AIRPORTS_JSON_FILE_PATH = "./airports.json"
+AIRCRAFT_FUEL_CONSUMPTION_JSON_FILE_PATH = "./aircraft_fuel_consumption_rates.json"
 
 
 def load_airport_locations(data_file_path: str = AIRPORTS_JSON_FILE_PATH) -> dict:
@@ -16,9 +18,24 @@ def load_airport_locations(data_file_path: str = AIRPORTS_JSON_FILE_PATH) -> dic
             if "lat" in airport and "lon" in airport}
 
 
+def load_aircraft_fuel_consumption_rates(data_file_path: str = AIRCRAFT_FUEL_CONSUMPTION_JSON_FILE_PATH) -> dict:
+
+    with open(data_file_path) as f:
+        aircraft_data = json.load(f)
+
+
 def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
-    """Calculates distance between two locations """
-    pass
+    """Calculates distance between two locations. The latitudes and longitudes are 
+    converted to radians and the distance is calculated using the Haversine formula. 
+    r is the radius of the earth."""
+
+    r = 6371
+    lat1_rad = lat1 * pi/180
+    lat2_rad = lat2 * pi/180
+    lon1_rad = lon1 * pi/180
+    lon2_rad = lon2 * pi/180
+
+    return acos(sin(lat1_rad)*sin(lat2_rad) + cos(lat1_rad)*cos(lat2_rad)*cos(lon2_rad-lon1_rad)) * r
 
 
 def find_nearest_airport(lat: float, lon: float, airport_locations: dict) -> str:
@@ -27,4 +44,4 @@ def find_nearest_airport(lat: float, lon: float, airport_locations: dict) -> str
 
 if __name__ == "__main__":
 
-    print(load_airport_locations())
+    print(haversine_distance(40.7486, 62.5, 21.89, 86))
