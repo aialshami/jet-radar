d4508589bfe4da9c317057659aeda96a724bbe98
aialshami <a.i.alshami@gmail.com>
2023-05-22 16:19:52 +0100

    added variables

---
+++ terraform_pipeline/lambda.tf
@@ -0,0 +1,152 @@
+# Creata lambda IAM policy
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data aws_iam_policy_document lambda_s3 {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListObjects"
+    ]
+    resources = [
+      "arn:aws:s3:::jet-bucket/*"
+    ]
+  }
+}
+
+resource aws_iam_policy lambda_s3 {
+  name        = "lambda-s3-permissions"
+  description = "Contains S3 put permission for lambda"
+  policy      = data.aws_iam_policy_document.lambda_s3.json
+}
+
+# Creata lambda IAM role
+resource "aws_iam_role" "jet_iam_lambda" {
+  name = "jet_iam_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource aws_iam_role_policy_attachment lambda_policy_attach {
+    role = aws_iam_role.jet_iam_lambda.name
+    policy_arn = aws_iam_policy.lambda_s3.arn
+}
+
+# Create staging lambda function that will run every 10 mins
+resource "aws_lambda_function" "jet_staging_lambda" {
+  function_name = "jet_staging_lambda"
+  role          = aws_iam_role.jet_iam_lambda.arn
+  memory_size   = 1024
+  timeout       = 120
+  image_uri     = "605126261673.dkr.ecr.eu-west-2.amazonaws.com/jet-staging-repo:latest"
+  package_type  = "Image"
+  architectures = ["x86_64"]
+
+  environment {
+    variables = {
+        RAPIDAPI_KEY=var.rapidapi_key
+        RAPIDAPI_HOST=var.rapidapi_host
+
+        DB_HOST=aws_db_instance.jet_db.endpoint
+        DB_NAME=var.db_name
+        DB_USER=var.username
+        DB_PASSWORD=var.password
+        DB_PORT=var.port
+
+        STAGING_TABLE_NAME=var.staging_table
+        STAGING_SCHEMA=var.staging_schema
+
+        ACCESS_KEY=var.aws_access_key
+        SECRET_KEY=var.aws_secret_key
+
+        S3_BUCKET_NAME=aws_s3_bucket.jet_bucket.bucket
+        CELEB_INFO=var.celeb_info
+    }
+  }
+}
+
+# Creates cloudwatch event
+resource "aws_cloudwatch_event_rule" "jet_ten_mins" {
+  name                = "jet_ten_mins"
+  schedule_expression = "rate(10 minutes)"
+
+}
+
+# Create staging schedule target for lambda function 
+resource "aws_cloudwatch_event_target" "jet_target_ten_mins" {
+  rule = aws_cloudwatch_event_rule.jet_ten_mins.name
+  arn  = aws_lambda_function.jet_staging_lambda.arn
+}
+
+# Create staging trigger prevention
+resource "aws_lambda_permission" "allow_call_jet_ten_mins" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.jet_staging_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.jet_ten_mins.arn
+}
+
+# Create production lambda function that will run every 24 hours
+resource "aws_lambda_function" "jet_production_lambda" {
+  function_name = "jet_production_lambda"
+  role          = aws_iam_role.jet_iam_lambda.arn
+  memory_size   = 1024
+  timeout       = 120
+  image_uri     = "605126261673.dkr.ecr.eu-west-2.amazonaws.com/jet-production-repo:latest"
+  package_type  = "Image"
+  architectures = ["x86_64"]
+
+  environment {
+    variables = {
+        RAPIDAPI_KEY=var.rapidapi_key
+        RAPIDAPI_HOST=var.rapidapi_host
+
+        DB_HOST=aws_db_instance.jet_db.endpoint
+        DB_NAME=var.db_name
+        DB_USER=var.username
+        DB_PASSWORD=var.password
+        DB_PORT=var.port
+
+        STAGING_TABLE_NAME=var.staging_table
+        STAGING_SCHEMA=var.staging_schema
+        PRODUCTION_SCHEMA=var.production_schema
+
+        ACCESS_KEY=var.aws_access_key
+        SECRET_KEY=var.aws_secret_key
+
+        S3_BUCKET_NAME=aws_s3_bucket.jet_bucket.bucket
+        CELEB_INFO=var.celeb_info
+    }
+  }
+}
+
+# Creates cloudwatch event
+resource "aws_cloudwatch_event_rule" "jet_daily" {
+  name                = "jet_daily"
+  schedule_expression = "rate(24 hours)"
+
+}
+
+# Create staging schedule target for lambda function 
+resource "aws_cloudwatch_event_target" "jet_target_daily" {
+  rule = aws_cloudwatch_event_rule.jet_daily.name
+  arn  = aws_lambda_function.jet_production_lambda.arn
+}
+
+# Create staging trigger prevention
+resource "aws_lambda_permission" "allow_call_daily" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.jet_production_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.jet_daily.arn
+}
