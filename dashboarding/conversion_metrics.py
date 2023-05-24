6420ee451218704170d13247e2fc091867d33e1f
kc-devacc <kyle.charlton.19@gmail.com>
2023-05-24 14:06:03 +0100

    stash

--- dashboarding/conversion_metrics.py
+++ dashboarding/conversion_metrics.py
@@ -128,6 +128,7 @@ def get_number_of_flights(name:str, owner_df:DataFrame, aircraft_df: DataFrame, 
 
 def get_new_infographic_text(previous: str):
     """ Changes the infographic text to the next visualisation emoji prompt """
+
     return f"previous was {previous} & next is CO2"
 
 
