af836117a9b8ec92bccbdea5320c10050ca3d92a
Oliver Jordan <o.jordan2308@gmail.com>
2023-05-24 16:57:15 +0100

    Added docstrings to extract tests

--- extract/test_extract.py
+++ extract/test_extract.py
@@ -8,7 +8,7 @@ from extract import get_flights_for_all_celebs, get_flight_params, get_celeb_jso
 
 
 def test_celebs_have_expected_plane_info(celeb_planes_data):
-    """"""
+    """Test the celeb data has fields needed for extract script."""
 
     for celeb_plane in celeb_planes_data:
         assert "icao_hex" in celeb_plane and "tail_number" in celeb_plane
@@ -25,7 +25,7 @@ def test_get_flights_for_celebs(mocked_flight_info, celeb_planes_data):
 
 
 def test_get_flight_params_raises_keyerror():
-    """"""
+    """Test function responds as expected to invalid input."""
 
     with pytest.raises(KeyError) as err:
         data = {}
@@ -35,7 +35,7 @@ def test_get_flight_params_raises_keyerror():
 
 
 def test_get_flight_params_unix_conversion():
-    """"""
+    """Test the get flight params function converts unix time correctly."""
 
     data = {"ac": [{}], "now": 1}
     assert get_flight_params(data)["time_input"] == datetime(1970, 1, 1, 0, 0, 0, 1000)
@@ -43,7 +43,9 @@ def test_get_flight_params_unix_conversion():
 
 @patch("boto3.resource")
 def test_get_celeb_json_from_s3(mocked_s3):
-    """"""
+    """Tests get celeb json extracts and loads file contents correctly using mocked boto3
+    s3 resource."""
+
     os.environ["S3_BUCKET_NAME"] = "test_bucket"
     os.environ["CELEB_INFO"] = "test"
 
@@ -59,12 +61,14 @@ def test_get_celeb_json_from_s3(mocked_s3):
 
 @patch("requests.get")
 def test_api_call(mock_request):
-    """"""
+    """Test the get current flight for icao function calls the api and reads the
+    response as expected using a mocked get request."""
+
     os.environ["RAPIDAPI_KEY"] = "shhhh"
     os.environ["RAPIDAPI_HOST"] = "it's secret"
 
     mock_request.return_value.json.return_value = "Success"
-    data = get_current_flight_for_icao("6zerozerozero")
+    data = get_current_flight_for_icao("6000")
 
     mock_request.assert_called()
     assert data == "Success"
