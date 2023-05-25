a6fdc2acb514a7848d6a90c458b18c61b7d34d84
ojordan2308 <112091373+ojordan2308@users.noreply.github.com>
2023-05-25 10:06:16 +0100

    Improve test_transform.py code quality

--- transform/test_transform.py
+++ transform/test_transform.py
@@ -4,8 +4,9 @@ from unittest.mock import patch, MagicMock
 from psycopg2.extensions import connection
 from psycopg2.extras import RealDictCursor
 import pandas as pd
-from transform import extract_todays_flights
+
 from utilities import haversine_distance, find_nearest_airport, calculate_fuel_consumption
+from transform import extract_todays_flights
 
 
 def test_antipodal_haversine_distance_is_half_circumference_of_earth():
@@ -27,21 +28,21 @@ def test_haversine_distance():
     assert round(haversine_distance(-18, 35, 72, -40)) == 11407
 
 
-def test_find_nearest_airport_BER(airport_data):
+def test_find_nearest_airport_ber(airport_data):
     """Checks that when the lat/lon of BER is input to the find_nearest_airport
     function, BER is returned."""
 
     assert find_nearest_airport(52.36, 13.51, airport_data) == "BER"
 
 
-def test_find_nearest_airport_WKK(airport_data):
+def test_find_nearest_airport_wkk(airport_data):
     """Checks that when the lat/lon of WKK is input to the find_nearest_airport
     function, WKK is returned."""
 
     assert find_nearest_airport(59.3, -158.61, airport_data) == "WKK"
 
 
-def test_fuel_usage_of_LJ40_over_one_hour(aircraft_data):
+def test_fuel_usage_of_lj40_over_one_hour(aircraft_data):
     """Tests the fuel usage of LJ40 jet over a single hour."""
 
     test_dep_time = datetime.now() - timedelta(hours=1)
@@ -49,7 +50,7 @@ def test_fuel_usage_of_LJ40_over_one_hour(aircraft_data):
     assert calculate_fuel_consumption(test_dep_time, test_arr_time, "LJ40", aircraft_data) == 207
 
 
-def test_fuel_usage_of_GA5C(aircraft_data):
+def test_fuel_usage_of_ga5c(aircraft_data):
     """Tests the fuel usage of GA5C jet over 7 hours 43 minutes and 27 seconds."""
 
     test_dep_time = datetime.now() - timedelta(hours=7, minutes=42, seconds=87)
